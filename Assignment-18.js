70f13a263426b0e8f845205872340b05edb1ed23
RAJNEE <rajni.gla_cs18@gla.ac.in>
2020-12-06 19:37:01 -0800

    Assignment-18

--- Assignment-18.js
+++ Assignment-18.js
@@ -1,87 +1,78 @@
-//selector
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
-const small = document.querySelector('small');
+const psw = document.getElementById('psw');
+const psw1 = document.getElementById('psw1');
 
-//event
 form.addEventListener('submit',function(e){
-    e.preventDefault();
+    e.preventDefault(); 
     checkInput();
 });
 
-//function
-function checkInput()
-{
-  const usernameValue=username.value.trim();
-  const emailValue=email.value.trim();
-  const passwordValue=password.value.trim();
-  const password2Value=password2.value.trim();
-  //username
-  if(usernameValue === '')  
-  {
-      showError(username,"Username can not be Blank");
-  }
-  else if(!isUsernameValid(usernameValue))
+function checkInput(){
+    const usernameValue =  username.value.trim();
+    const emailValue =  email.value.trim();
+    const password1Value =  psw.value.trim();
+    const password2Value =  psw1.value.trim();
+
+    if( usernameValue ==='')
     {
-        showError(username, "Username can have small letters and underscores only");
+        showError(username,"Username can not be blank");
     }
-  else
-  {
-      showSuccess(username);
-  }
-  //email
-
-  if(emailValue === '')
-  {
-      showError(email,"Email can not be Blank");
-  }
-  else if(!isEmailValid(emailValue))
+    else
     {
-    showError(email,"Email id is not be Valid");
+        showSuccess(username);
+    } 
+    if( emailValue ==='')
+    {
+        showError(email,"Email Id can not be blank");
     }
-   else{
-    showSuccess(email);
+    else if(!isEmailValid(emailValue))
+    {
+        showError(email,"Email is not Valid");
+    }
+    else
+    {
+        showSuccess(email);
+    }    
+
+    if( password1Value ==='')
+    {
+        showError(psw,"Password can not be blank");
+    }
+    else
+    {
+        showSuccess(psw);
     }
 
-//password
-if(passwordValue===''){
-    showError(password1, "Password can't be blank");
+    if( password2Value ==='')
+    {
+        showError(psw1,"Password can not be blank");
+    }
+    else if(password2Value != password1Value)
+    {
+        showError(psw1,"Passwords not matched");
+    }
+    else
+    {
+        showSuccess(psw1);
+    } 
+
 }
-else {
-    showSuccess(password);
-}
-if(password2Value===''){
-    showError(password2, "Re-enter password");
-}
-else if(password2Value != passwordValue)
+
+function showError(input,msg)
 {
-    showError(password2, "Password not matched!")
-}
-else {
-    showSuccess(password2);
-}
-
-}
-function showError(input,msg){
     const formControl = input.parentNode;
-    formControl.className = 'form-control error';
+    formControl.className = 'form-container error';
     const small = formControl.querySelector('small');
     small.innerHTML = msg;
-
 }
-function showSuccess(input,msg){
-    const formControl = input.parentNode;
-    formControl.className = 'form-control success';
-    
-}
-
-function isEmailValid(email)
+function showSuccess(input)
 {
-    return /^([a-zA-Z0-9_\.\-\+]+)@([a-zA-Z0-9]+)\.([a-zA-Z]{2,3})$/.test(email);
+    const formControl = input.parentNode;
+    formControl.className = 'form-container success';
 }
-function isUsernameValid(username){
-    return /^[a-z_]+$/.test(username);
+
+function isEmailValid(email1){
+    return /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9]+)\.([a-zA-Z]{2,3})$/.test(email1);
 }
